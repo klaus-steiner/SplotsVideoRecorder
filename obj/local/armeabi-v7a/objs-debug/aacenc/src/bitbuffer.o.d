2cb5d5bf963539c60a0f4542a8a5f2836a456edc
Klaus Steiner <klaus@splots.co>
2014-02-21 21:27:36 +0100

    git ignore

--- obj/local/armeabi-v7a/objs-debug/aacenc/src/bitbuffer.o.d
+++
@@ -1,9 +0,0 @@
-obj/local/armeabi-v7a/objs-debug/aacenc/src/bitbuffer.o: \
- jni/libaacenc/src/bitbuffer.c jni/libaacenc/inc/bitbuffer.h \
- jni/libaacenc/inc/typedef.h jni/libaacenc/inc/typedefs.h
-
-jni/libaacenc/inc/bitbuffer.h:
-
-jni/libaacenc/inc/typedef.h:
-
-jni/libaacenc/inc/typedefs.h:
--- obj/local/armeabi/objs-debug/aacenc/src/bitbuffer.o.d
+++
@@ -1,9 +0,0 @@
-obj/local/armeabi/objs-debug/aacenc/src/bitbuffer.o: \
- jni/libaacenc/src/bitbuffer.c jni/libaacenc/inc/bitbuffer.h \
- jni/libaacenc/inc/typedef.h jni/libaacenc/inc/typedefs.h
-
-jni/libaacenc/inc/bitbuffer.h:
-
-jni/libaacenc/inc/typedef.h:
-
-jni/libaacenc/inc/typedefs.h:
